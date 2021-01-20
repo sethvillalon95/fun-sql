22fdc3fde7941a6c329af3070d68fe247955b7cf
Seth Villalon <sethvillalon95@gmail.com>
2021-01-19 20:50:11 -1000

    Finish Project

--- fun-sql/src/Main.java
+++ fun-sql/src/Main.java
@@ -12,6 +12,7 @@ public class Main extends JFrame {
 
     private Vis mainPanel;
 
+
     public Main() {
 
         JMenuBar mb = setupMenu();
@@ -54,29 +55,109 @@ public class Main extends JFrame {
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
         JMenuItem item1 = new JMenuItem("Item 1");
-        JMenu subMenu = new JMenu("Submenu");
         JMenuItem item2 = new JMenuItem("Item 2");
+        JMenuItem item3 = new JMenuItem("Item 3");
+        JMenuItem item4 = new JMenuItem("Item 4");
+        JMenuItem item5 = new JMenuItem("Item 5");
+        JMenuItem item6 = new JMenuItem("Item 6");
+        JMenuItem item7 = new JMenuItem("Item 7");
+
+
+        
+        JMenu subMenu = new JMenu("Submenu");
+//        JMenuItem item2 = new JMenuItem("Item 2");
 
         //setup action listeners
+        
+        // All the students 
         item1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Just clicked menu item 1");
                 int gilmo = runSimpleCountQuery("SELECT COUNT(*) FROM cis2019");
-                System.out.println("I found " + gilmo + " rows in the table.");
+                System.out.println("I found " + gilmo + " students in the table.");
                 mainPanel.setText("I found " + gilmo + " rows in the table.");
             }
         });
+        
+        //All CS Students
         item2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("Just clicked menu item 2");
+                int output = runSimpleCountQuery("SELECT COUNT(*) FROM cis2019 WHERE  major = 'Computer Science'");
+                System.out.println("I found " + output + " Computer Science Students in the table.");
+                mainPanel.setText("I found " + output + " Computer Science Students in the table.");
+            }
+        });
+        
+        // All IS Students
+        item3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Just clicked menu item 3");
+                int output = runSimpleCountQuery("SELECT COUNT(*) FROM cis2019 WHERE  major = 'Information Systems'");
+                System.out.println("I found " + output + " Information Systems Students in the table.");
+                mainPanel.setText("I found " + output + " Information Systems Students in the table.");
+            }
+        });
+        
+    
+        // All IT Student
+        item4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Just clicked menu item 4");
+                int output = runSimpleCountQuery("SELECT COUNT(*) FROM cis2019 WHERE  major = 'Information Technology'");
+                System.out.println("I found " + output + " Information Technology Students in the table.");
+                mainPanel.setText("I found " + output + " Information Technology Students in the table.");
+            }
+        });
+        
+        // CS or IT students
+        item5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Just clicked menu item 5");
+                int output = runSimpleCountQuery("SELECT COUNT(*) FROM cis2019 WHERE  major = 'Information Technology' OR major = 'Computer Science'");
+                System.out.println("I found " + output + " Information Technology and Computer Science Students in the table.");
+                mainPanel.setText("I found " + output + " Information Technology and Computer Science Students in the table.");
+            }
+        });
+        
+        // Female Computer Science 
+        item6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Just clicked menu item 6");
+                int output = runSimpleCountQuery("SELECT COUNT(*) FROM cis2019 WHERE  major = 'Computer Science' AND gender = 'F'");
+                System.out.println("I found " + output + " Female Computer Science Students in the table.");
+                mainPanel.setText("I found " + output + " Female Computer Science Students in the table.");
+            }
+        });
+        
+        // Male Computer Science 
+        item7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Just clicked menu item 7");
+                int output = runSimpleCountQuery("SELECT COUNT(*) FROM cis2019 WHERE  major = 'Computer Science' AND gender = 'M'");
+                System.out.println("I found " + output + " Male Computer Science Students in the table.");
+                mainPanel.setText("I found " + output + " Male Computer Science Students in the table.");
             }
         });
 
+
         //now hook them all together
-        subMenu.add(item2);
+//        subMenu.add(item2);
+
         fileMenu.add(item1);
+        fileMenu.add(item2);
+        fileMenu.add(item3);
+        fileMenu.add(item4);
+        fileMenu.add(item5);
+        fileMenu.add(item6);
+        fileMenu.add(item7);
         fileMenu.add(subMenu);
         menuBar.add(fileMenu);
 
